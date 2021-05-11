8abee19f2b5be1ec9538e21925c11e4aea2e4e44
mac <mac@macs-MacBook-Air-4.local>
2021-05-12 00:03:59 +0100

    update with templates contain index and about me page wiith other statics files

--- my_portfolios/portfolios/views.py
+++ my_portfolios/portfolios/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
+from .models import PersonalData,Projects
 # Create your views here.
 
+
+
 def index(request):
-    return HttpResponse('this is my porfolio website')
+    personal_datas = PersonalData.objects 
+    projects = Projects.objects
+    return render(request,'portfolios/index.html',{'personal_data':personal_datas, 'projects':projects})
+
+def about(request):
+    return render(request, 'portfolios/about.html')
+
+
+    
